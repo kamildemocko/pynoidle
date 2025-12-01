76c8be237a5c38405f16a887ca49498d42e76602
Kamil Democko <kamil.democko@gmail.com>
2025-12-01 16:53:24 +0100

    usage

--- pynoidle/arguments.py
+++ pynoidle/arguments.py
@@ -10,9 +10,15 @@ class Args:
 
 def parse_args() -> Args:
     args_parser = ArgumentParser(description="utility to prevent idle by pressing F13 button")
-    args_parser.add_argument("--delay", "-d", type=int, help="delay between presses", default=60, dest="delay")
-    args_parser.add_argument("--no-start", "-s", action="store_false", help="start the pressing at start-up", dest="start")
+    args_parser.add_argument("-d", "--delay", type=int, help="delay between presses", default=60, dest="delay")
+    args_parser.add_argument("-s", "--no-start", action="store_false", help="start the pressing at start-up", dest="start")
     args_parser.set_defaults(start=True)
-    args = args_parser.parse_args()
+
+    try:
+        args = args_parser.parse_args()
+    except SystemExit as see:
+        if see.code != 0:
+            args_parser.print_usage()
+        raise
 
     return Args(delay=args.delay, start=args.start)
